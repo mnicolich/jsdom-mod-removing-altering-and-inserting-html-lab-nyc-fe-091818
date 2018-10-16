578b3583610215c051cc732a5de93315378482bf
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-16 23:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,20 +2,3 @@
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
-document.createElement(tagName)
-document.body.appendChild(element)
-var ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
-element.innerHTML = 'Hello, DOM'
-element.style.backgroundColor = '#ff0090';
-element.style.textAlign = 'center';
-ul.style.textAlign = 'left'
-ul.removeChild(ul.querySelector('li:nth-child(2)'))
-ul.remove()
