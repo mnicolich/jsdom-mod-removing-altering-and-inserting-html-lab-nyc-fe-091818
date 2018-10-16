1bed7b19cf060b3a1a4a9d2b250ffff4797a4979
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-16 23:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 document.querySelector('main').remove()
+let pTag = document.querySelector('div p')
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = victory
