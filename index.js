cdab71d3977fc7d5c6a610d4f88669508cad3f75
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-17 00:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* Enter the code to remove the main node element under this comment */
-document.querySelector('#main').remove()
 main.remove()
 
 /* Create your new element here and assign it to newHeader */
