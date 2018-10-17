ce786cac623e69062650810ac244e12a5a213663
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-17 00:32:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const header = document.createElement('h1')
+header.id = 'victory'
+header.innerText = 'Melissa is the champion!'
+document.body.appendChild(header)
+const newHeader = document.querySelector('#victory');
+console.log(newHeader)
